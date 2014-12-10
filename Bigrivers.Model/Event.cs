ded1ce7d4b9612d5b031ceb6dcb232b1b08fb1ef
rikproDVC <99016312@mydavinci.nl>
2014-12-10 12:10:26 +0100

    Added models for event, genre, deleted obsolete model for show

---
+++ Bigrivers.Model/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bigrivers.Model
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public decimal Price { get; set; }
+        public bool TicketRequired { get; set; }      
+        public bool Status { get; set; }
+        public List<Performance> Performances { get; set; }
+        public virtual Location Location { get; set; }
+
+    }
+}
