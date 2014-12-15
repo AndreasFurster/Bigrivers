ce662b794d01548bf619cbc2f937eb181762a7e3
AndreasFurster <andreas.furster@hotmail.com>
2014-12-15 10:16:57 +0100

    Revert "Revert "Revert "Start implementing OData to the webservice"""
    This reverts commit 9a6c14a21f40514e1530f0922b38d8e3d7e2d42c.

--- Bigrivers.Server/Bigrivers.Server.Webservice/App_Start/WebApiConfig.cs
+++ Bigrivers.Server/Bigrivers.Server.Webservice/App_Start/WebApiConfig.cs
@@ -2,10 +2,6 @@
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using System.Net.Http.Formatting;
-using System.Web.OData.Builder;
-using System.Web.OData.Extensions;
-using Bigrivers.Server.Model;
-using Microsoft.OData.Edm;
 
 namespace Bigrivers.Server.Webservice
 {
@@ -13,27 +9,7 @@ namespace Bigrivers.Server.Webservice
     {
         public static void Register(HttpConfiguration config)
         {
-            //// Web API configuration and services
-
-            //// Web API routes
-            //config.MapHttpAttributeRoutes();
-
-            //config.Routes.MapHttpRoute(
-            //    name: "Api",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
-
-            //// Remove XML formatter to prevent XML output
-            //config.Formatters.Remove(config.Formatters.XmlFormatter);
-
-            //// Adding JSON formatter
-            //JsonMediaTypeFormatter jmf = config.Formatters.JsonFormatter;
-            //jmf.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-
-            //config.Formatters.Add(jmf);
-
-            /* ====== OData config ====== */
+            // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -44,27 +20,14 @@ namespace Bigrivers.Server.Webservice
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            config.MapODataServiceRoute(
-                routeName: "ODataRoute",
-                routePrefix: null,
-                model: GetModel()
-            );
-        }
+            // Remove XML formatter to prevent XML output
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
 
-        public static IEdmModel GetModel()
-        {
-            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            // Adding JSON formatter
+            JsonMediaTypeFormatter jmf = config.Formatters.JsonFormatter;
+            jmf.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
 
-            builder.EntitySet<Artist>("Artist");
-            builder.EntitySet<Event>("Event");
-            builder.EntitySet<Genre>("Genre");
-            builder.EntitySet<Location>("Location");
-            builder.EntitySet<NewsItem>("NewsItem");
-            builder.EntitySet<Page>("Page");
-            builder.EntitySet<Performance>("Performance");
-            builder.EntitySet<Sponsor>("Sponsor");
-
-            return builder.GetEdmModel();
+            config.Formatters.Add(jmf);
         }
     }
 }
