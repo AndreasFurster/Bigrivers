ca43992521f5bb680e778459fdc85a39c7d501e0
rikproDVC <99016312@mydavinci.nl>
2015-01-13 12:12:25 +0100

    Added new genres, locations and artists, started adding performances

--- Bigrivers.Server/Bigrivers.Server.Data/Migrations/Configuration.cs
+++ Bigrivers.Server/Bigrivers.Server.Data/Migrations/Configuration.cs
@@ -40,6 +40,31 @@ namespace Bigrivers.Server.Data.Migrations
                 Name = "Blues",
                 Artists = new List<Artist>()
             };
+            Genre Techno = new Genre
+            {
+                Name = "Techno",
+                Artists = new List<Artist>()
+            };
+            Genre Metal = new Genre
+            {
+                Name = "Metal",
+                Artists = new List<Artist>()
+            };
+            Genre Klassiek = new Genre
+            {
+                Name = "Klassiek",
+                Artists = new List<Artist>()
+            };
+            Genre Nederlands = new Genre
+            {
+                Name = "Nederlands",
+                Artists = new List<Artist>()
+            };
+            Genre KPop = new Genre
+            {
+                Name = "K-Pop",
+                Artists = new List<Artist>()
+            };
 
             // Create Location
             Location Sterrenburg = new Location
@@ -47,7 +72,34 @@ namespace Bigrivers.Server.Data.Migrations
                 Street = "Dordtsestraat",
                 Zipcode = "1234 CS",
                 City = "Dordrecht",
-                Stagename = "Sterrenburg",
+                Stagename = "Sterrenburg Stage",
+                Status = true,
+                Events = new List<Event>()
+            };
+            Location LPP = new Location
+            {
+                Street = "Leerparkpromenade",
+                Zipcode = "5643 LP",
+                City = "Dordrecht",
+                Stagename = "Davinci Stage",
+                Status = true,
+                Events = new List<Event>()
+            };
+            Location Haven = new Location
+            {
+                Street = "Dordtsehaven",
+                Zipcode = "5565 CS",
+                City = "Dordrecht",
+                Stagename = "Haven Stage",
+                Status = true,
+                Events = new List<Event>()
+            };
+            Location DordtCentraal = new Location
+            {
+                Street = "Dordt Centraal",
+                Zipcode = "3240 JS",
+                City = "Dordrecht",
+                Stagename = "Jonkheer Stage",
                 Status = true,
                 Events = new List<Event>()
             };
@@ -56,7 +108,7 @@ namespace Bigrivers.Server.Data.Migrations
             Artist Queen = new Artist
             {
                 Name = "Queen",
-                Description = "Best group ever (to some people)",
+                Description = "Queen is een Engelse rockgroep. De band is opgericht in 1970 in Londen door gitarist Brian May, zanger Freddie Mercury en drummer Roger Taylor, aangevuld met bassist John Deacon in 1971. Met tientallen hits in de jaren 70, 80 en 90, is Queen een van de succesvolste popgroepen in de geschiedenis.",
                 Avatar = null,
                 YoutubeChannel = "https://www.youtube.com/user/queenofficial",
                 Website = "http://www.queenonline.com/",
@@ -70,7 +122,7 @@ namespace Bigrivers.Server.Data.Migrations
             Artist JusBie = new Artist
             {
                 Name = "Justin Bieber",
-                Description = "All the teenage girls!!!",
+                Description = "Justin Bieber is een Canadees zanger. Hij begon als kleuter met drummen en gitaarspelen en zong voor het eerst in het openbaar toen hij twaalf jaar oud was. Twee jaar later, nadat hij aan de hand van een video op YouTube was ontdekt, tekende hij zijn eerste platencontract, en groeide vervolgens uit tot een tieneridool.",
                 Avatar = null,
                 YoutubeChannel = "https://www.youtube.com/user/JustinBieberVEVO",
                 Website = "http://www.justinbiebermusic.com",
@@ -84,10 +136,153 @@ namespace Bigrivers.Server.Data.Migrations
             Artist BruMar = new Artist
             {
                 Name = "Bruno Mars",
-                Description = "Lazy Billionaire",
+                Description = "Peter Hernandez, beter bekend als Bruno Mars, is een Amerikaans zanger, schrijver en muziekproducent.",
                 Avatar = null,
                 YoutubeChannel = "https://www.youtube.com/user/ElektraRecords",
                 Website = "http://www.brunomars.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist Psy = new Artist
+            {
+                Name = "Psy",
+                Description = "Psy - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/Psyofficial",
+                Website = "http://www.psyofficial.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist Coldplay = new Artist
+            {
+                Name = "Coldplay",
+                Description = "Coldplay - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/ColdplayVEVO",
+                Website = "http://www.coldplay.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist NicSim = new Artist
+            {
+                Name = "Nick & Simon",
+                Description = "Nick & Simon - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/NickSimonNL",
+                Website = "http://www.nicksimon.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist Jamai = new Artist
+            {
+                Name = "Jamai",
+                Description = "Jamai - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/JamaiNL",
+                Website = "http://www.jamainl.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist BluesBros = new Artist
+            {
+                Name = "Blues Brothers",
+                Description = "The Blues Brothers, more formally called The Blues Brothers' Show Band and Revue, are an American blues and rhythm and blues revivalist band founded in 1978 by comedy actors Dan Aykroyd and John Belushi as part of a musical sketch on Saturday Night Live",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/BluesBrothers",
+                Website = "http://www.bluesbrothers.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist ArmVBuu = new Artist
+            {
+                Name = "Armin Van Buuren",
+                Description = "Armin van Buuren - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/arminvanbuuren",
+                Website = "http://www.arminvanbuuren.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist Bastille = new Artist
+            {
+                Name = "Bastille",
+                Description = "Bastille - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/Bastille",
+                Website = "http://www.Bastille.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist FooFight = new Artist
+            {
+                Name = "Foo Fighters",
+                Description = "Foo Fighters - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/FooFighters",
+                Website = "http://www.FooFighters.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist NieGeu = new Artist
+            {
+                Name = "Niels Geusebroek",
+                Description = "Niels Geusebroek - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/NielsGeusebroek",
+                Website = "http://www.nielsgeusebroek.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist Pitbull = new Artist
+            {
+                Name = "Pitbull",
+                Description = "Pitbull - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/PitbullVEVO",
+                Website = "http://www.pitbullmusic.com/",
+                Facebook = null,
+                Twitter = null,
+                Status = true,
+                Performances = new List<Performance>(),
+                Genres = new List<Genre>()
+            };
+            Artist Script = new Artist
+            {
+                Name = "The Script",
+                Description = "The Script - Beschrijving",
+                Avatar = null,
+                YoutubeChannel = "https://www.youtube.com/user/Jeroenvdboom",
+                Website = "http://www.jeroenboom.com/",
                 Facebook = null,
                 Twitter = null,
                 Status = true,
@@ -99,8 +294,8 @@ namespace Bigrivers.Server.Data.Migrations
             Event TGF = new Event
             {
                 Title = "Teen Girl Fest",
-                Description = "Everything an annoying preteen girl likes",
-                ShortDescription = "Everything an annoying preteen girl likes",
+                Description = "Music for the younger girls, like Justin Bieber and One Direction",
+                ShortDescription = "Music for the younger girls",
                 Start = DateTime.Now,
                 End = DateTime.Now.AddDays(1),
                 Price = 13.37m,
@@ -109,12 +304,35 @@ namespace Bigrivers.Server.Data.Migrations
                 Performances = new List<Performance>(),
                 Location = Sterrenburg
             };
-
+            Event BigEverything = new Event
+            {
+                Title = "BigEverything",
+                Description = "All kinds of unrelated music stuffed together! Cause why not?",
+                ShortDescription = "All kinds of unrelated music!",
+                Start = DateTime.Now.AddDays(1),
+                End = DateTime.Now.AddDays(2),
+                Price = 13.37m,
+                TicketRequired = true,
+                Status = true,
+                Performances = new List<Performance>(),
+            };
             Event BreakingFree = new Event
             {
                 Title = "Breaking Free",
-                Description = "One of the best groups ever here!",
-                ShortDescription = "One of the best groups ever here!",
+                Description = "All kinds of 60's and 70's music, with a main act from Queen!",
+                ShortDescription = "All kinds of 60's and 70's music",
+                Start = DateTime.Now.AddDays(1),
+                End = DateTime.Now.AddDays(2),
+                Price = 13.37m,
+                TicketRequired = true,
+                Status = true,
+                Performances = new List<Performance>(),
+            };
+            Event DutchParty = new Event
+            {
+                Title = "Dutch Party",
+                Description = "Nederlandse artiesten zoals Jeroen vd Boom, Nick & Simon bij elkaar!",
+                ShortDescription = "Nederlandse artiesten bij elkaar!",
                 Start = DateTime.Now.AddDays(1),
                 End = DateTime.Now.AddDays(2),
                 Price = 13.37m,
@@ -123,17 +341,48 @@ namespace Bigrivers.Server.Data.Migrations
                 Performances = new List<Performance>(),
             };
 
+            Event LiveDance = new Event
+            {
+                Title = "Live Dance",
+                Description = "Dancing music on the big stage!",
+                ShortDescription = "Dancing music!",
+                Start = DateTime.Now.AddDays(1),
+                End = DateTime.Now.AddDays(2),
+                Price = 13.37m,
+                TicketRequired = true,
+                Status = true,
+                Performances = new List<Performance>(),
+            };
+
+
             // Create Performances
-            Performance QueenBreakingFree = new Performance
+            Performance QueenBreakingFreeP1 = new Performance
             {
                 Description = "Queen live at Breaking Free",
-                Start = BreakingFree.Start,
-                End = BreakingFree.End,
+                Start = BreakingFree.Start.AddDays(30),
+                End = BreakingFree.Start.AddDays(30).AddHours(1),
                 Status = true,
                 Artist = Queen,
                 Event = BreakingFree
             };
-
+            Performance BluesBrosBreakingFree = new Performance()
+            {
+                Description = "Blues Brothers at Breaking Free event",
+                Start = QueenBreakingFreeP1.End,
+                End = QueenBreakingFreeP1.End.AddHours(0.5),
+                Status = true,
+                Artist = BluesBros,
+                Event = BreakingFree
+            };
+            Performance QueenBreakingFreeP2 = new Performance
+            {
+                Description = "Queen live at Breaking Free for the second time the evening",
+                Start = BluesBrosBreakingFree.End,
+                End = BluesBrosBreakingFree.End.AddHours(1.5),
+                Status = true,
+                Artist = Queen,
+                Event = BreakingFree
+            };
             Performance BieberFest = new Performance
             {
                 Description = "Justin Bieber does stuff",
@@ -144,13 +393,19 @@ namespace Bigrivers.Server.Data.Migrations
                 Event = TGF
             };
 
-            Queen.Performances.Add(QueenBreakingFree);
+            Queen.Performances.Add(QueenBreakingFreeP1);
             Queen.Genres.Add(Rock);
 
             JusBie.Performances.Add(BieberFest);
             JusBie.Genres.Add(Pop);
 
             BruMar.Genres.Add(Pop);
+
+            Script.Genres.Add(Pop);
+
+            BluesBros.Genres.Add(Blues);
+
+
 
             BreakingFree.Performances.Add(QueenBreakingFree);
             TGF.Performances.Add(BieberFest);
