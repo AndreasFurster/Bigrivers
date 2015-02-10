d5dde238130772b1a3c4aa32f0248ff555df9cdb
AndreasFurster <andreas.furster@hotmail.com>
2015-02-10 11:41:22 +0100

    Change Owin Startup class to make an userManager and user for each request

--- Bigrivers.Server/Bigrivers.Server.Webservice/App_Start/Startup.cs
+++ Bigrivers.Server/Bigrivers.Server.Webservice/App_Start/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using System.Data.Entity;
+using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 using Bigrivers.Server.Data;
 using Bigrivers.Server.Webservice;
+using Bigrivers.Server.Webservice.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Cors;
+using Newtonsoft.Json.Serialization;
 using Owin;
 
 [assembly: OwinStartup(typeof(Startup))]
@@ -17,8 +21,31 @@ namespace Bigrivers.Server.Webservice
             var config = new HttpConfiguration();
             Database.SetInitializer<BigriversDb>(null);
 
-            WebApiConfig.Register(config);
+            ConfigureAuth(app);
+
+            ConfigureWebApi(config);
+
             app.UseCors(CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(IdentityDb.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            // Plugin the OAuth bearer tokens generation and Consumption will be here
+
+        }
+
+        private void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
