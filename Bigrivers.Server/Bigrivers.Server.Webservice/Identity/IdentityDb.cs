70ced14fd578cab755d2af28998462f16e754952
AndreasFurster <andreas.furster@hotmail.com>
2015-02-10 11:39:41 +0100

    Added userModel and EF context for Identity

---
+++ Bigrivers.Server/Bigrivers.Server.Webservice/Identity/IdentityDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Bigrivers.Server.Webservice.Identity
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb()
+            : base("BigriversContext", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    } 
+}
