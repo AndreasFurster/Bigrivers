225d70016d61a91a6e7a51b71dd8790b0397e290
AndreasFurster <andreas.furster@hotmail.com>
2015-02-11 22:04:30 +0100

    Added change password and delete user

--- Bigrivers.Server/Bigrivers.Server.Webservice/Controllers/AccountsController.cs
+++ Bigrivers.Server/Bigrivers.Server.Webservice/Controllers/AccountsController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Bigrivers.Server.Webservice.Identity;
@@ -10,42 +7,39 @@ using Microsoft.AspNet.Identity;
 
 namespace Bigrivers.Server.Webservice.Controllers
 {
-    [RoutePrefix("api/accounts")]
+    [RoutePrefix("accounts")]
     public class AccountsController : BaseApiController
     {
-
         [Route("users")]
         public IHttpActionResult GetUsers()
         {
-            return Ok(this.AppUserManager.Users.ToList().Select(u => this.TheModelFactory.Create(u)));
+            return Ok(AppUserManager.Users.ToList().Select(u => TheModelFactory.Create(u)));
         }
 
         [Route("user/{id:guid}", Name = "GetUserById")]
         public async Task<IHttpActionResult> GetUser(string Id)
         {
-            var user = await this.AppUserManager.FindByIdAsync(Id);
+            var user = await AppUserManager.FindByIdAsync(Id);
 
             if (user != null)
             {
-                return Ok(this.TheModelFactory.Create(user));
+                return Ok(TheModelFactory.Create(user));
             }
 
             return NotFound();
-
         }
 
         [Route("user/{username}")]
         public async Task<IHttpActionResult> GetUserByName(string username)
         {
-            var user = await this.AppUserManager.FindByNameAsync(username);
+            var user = await AppUserManager.FindByNameAsync(username);
 
             if (user != null)
             {
-                return Ok(this.TheModelFactory.Create(user));
+                return Ok(TheModelFactory.Create(user));
             }
 
             return NotFound();
-
         }
 
         [Route("create")]
@@ -56,26 +50,66 @@ namespace Bigrivers.Server.Webservice.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new ApplicationUser()
+            var user = new ApplicationUser
             {
                 UserName = createUserModel.Username,
                 Email = createUserModel.Email,
                 FirstName = createUserModel.FirstName,
                 LastName = createUserModel.LastName,
                 Level = 3,
-                JoinDate = DateTime.Now.Date,
+                JoinDate = DateTime.Now.Date
             };
 
-            IdentityResult addUserResult = await this.AppUserManager.CreateAsync(user, createUserModel.Password);
+            var addUserResult = await AppUserManager.CreateAsync(user, createUserModel.Password);
 
             if (!addUserResult.Succeeded)
             {
                 return GetErrorResult(addUserResult);
             }
 
-            Uri locationHeader = new Uri(Url.Link("GetUserById", new { id = user.Id }));
+            var locationHeader = new Uri(Url.Link("GetUserById", new {id = user.Id}));
 
             return Created(locationHeader, TheModelFactory.Create(user));
+        }
+
+        [Route("ChangePassword")]
+        public async Task<IHttpActionResult> ChangePassword(ChangePasswordBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result =
+                await
+                    AppUserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            return Ok();
+        }
+
+        [Route("user/{id:guid}")]
+        public async Task<IHttpActionResult> DeleteUser(string id)
+        {
+            var appUser = await AppUserManager.FindByIdAsync(id);
+
+            if (appUser != null)
+            {
+                var result = await AppUserManager.DeleteAsync(appUser);
+
+                if (!result.Succeeded)
+                {
+                    return GetErrorResult(result);
+                }
+
+                return Ok();
+            }
+
+            return NotFound();
         }
     }
 }
