722ce37ce53d3ea6f8cd2fa99ac9722e9957508c
NielsKlop <niels_klop@hotmail.com>
2015-03-23 09:32:22 +0100

    Changed MenuItem Model

--- src/Bigrivers.Server/Bigrivers.Server.Model/MenuItem.cs
+++ src/Bigrivers.Server/Bigrivers.Server.Model/MenuItem.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace Bigrivers.Server.Model
 {
+    public enum MenuItemType { ExternURL, FileURL, Page };
+
     class MenuItem
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Page { get; set; }
+        public string DisplayName { get; set; }
         public int Order { get; set; }
         public int Parent { get; set; }
+        public MenuItemType MenuItemType { get; set; }
+        public string Content { get; set; }
+
+        // ExternURL:   "http://www.crowdfunding.com/bigrivers"
+        // FileURL:     "/files/programma.pdf" ;  dit wijst naar een plek op de bigrivers webserver waar alle downloadbare files staan
+        // Page:        "http://www.bigrivers.nl/page</id>" => "[event(2)] <br> Dit is een mooi evenement. De volgende artiesten komen daar: <br> [event(2)\artists]"
     }
 }
